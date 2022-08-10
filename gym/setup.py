7bb4a74cfc22d36ae8b83c0351c74d5c3255c8a1
ikd04076 <78462512+ikd04076@users.noreply.github.com>
2022-08-10 18:00:40 +0900

    Update setup.py

--- gym/setup.py
+++ gym/setup.py
@@ -1,16 +1,24 @@
 from setuptools import setup
 
+# setup(name='f110_gym',
+#       version='0.2.1',
+#       author='Hongrui Zheng',
+#       author_email='billyzheng.bz@gmail.com',
+#       url='https://f1tenth.org',
+#       install_requires=['gym==0.19.0',
+#                         'numpy==1.20.0',
+#                         'Pillow>=8.3.2',
+#                         'scipy==1.7.3',
+#                         'numba==0.54.1',
+#                         'pyyaml>=5.4',
+#                         'pyglet',
+#                         'pyopengl']
+#       )
 setup(name='f110_gym',
-      version='0.2.1',
+      version='0.2',
       author='Hongrui Zheng',
       author_email='billyzheng.bz@gmail.com',
       url='https://f1tenth.org',
-      install_requires=['gym==0.19.0',
-                        'numpy==1.20.0',
-                        'Pillow>=8.3.2',
-                        'scipy==1.7.3',
-                        'numba==0.54.1',
-                        'pyyaml>=5.4',
-                        'pyglet',
-                        'pyopengl']
+      install_requires=[
+            'gym==0.18.0', 'numpy', 'Pillow', 'scipy', 'numba', 'pyyaml']
       )

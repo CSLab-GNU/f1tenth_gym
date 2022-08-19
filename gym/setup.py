f102721686d7ffe1593ff9c66ee520e8471c2a46
ikd04076 <78462512+ikd04076@users.noreply.github.com>
2022-08-19 11:31:56 +0900

    Update setup.py

--- gym/setup.py
+++ gym/setup.py
@@ -1,19 +1,3 @@
-# from setuptools import setup
-
-# setup(name='f110_gym',
-#       version='0.2.1',
-#       author='Hongrui Zheng',
-#       author_email='billyzheng.bz@gmail.com',
-#       url='https://f1tenth.org',
-#       install_requires=['gym==0.19.0',
-#                         'numpy==1.20.0',
-#                         'Pillow>=8.3.2',
-#                         'scipy==1.7.3',
-#                         'numba',
-#                         'pyyaml>=5.4',
-#                         'pyglet',
-#                         'pyopengl']
-#       )
 from setuptools import setup
 
 setup(name='f110_gym',
@@ -21,13 +5,29 @@ setup(name='f110_gym',
       author='Hongrui Zheng',
       author_email='billyzheng.bz@gmail.com',
       url='https://f1tenth.org',
-      package_dir={'': 'gym'},
       install_requires=['gym==0.19.0',
-		        'numpy<=1.22.0,>=1.18.0',
-                        'Pillow>=9.0.1',
-                        'scipy>=1.7.3',
+                        'numpy==1.20.0',
+                        'Pillow>=8.3.2',
+                        'scipy==1.7.3',
                         'numba>=0.55.2',
-                        'pyyaml>=5.3.1',
+                        'pyyaml>=5.4',
                         'pyglet',
                         'pyopengl']
       )
+# from setuptools import setup
+
+# setup(name='f110_gym',
+#       version='0.2.1',
+#       author='Hongrui Zheng',
+#       author_email='billyzheng.bz@gmail.com',
+#       url='https://f1tenth.org',
+#       package_dir={'': 'gym'},
+#       install_requires=['gym==0.19.0',
+# 		        'numpy<=1.22.0,>=1.18.0',
+#                         'Pillow>=9.0.1',
+#                         'scipy>=1.7.3',
+#                         'numba>=0.55.2',
+#                         'pyyaml>=5.3.1',
+#                         'pyglet',
+#                         'pyopengl']
+#       )
